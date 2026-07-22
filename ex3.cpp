27ce4ce1617f391a91b8a00614e2eabb023718a0
prajyot-sarade <prajyotsarade8@gmail.com>
2026-07-22 13:46:16 +0000

    add,sub,multi,divi,modulus

---
+++ ex3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+    cout<<"Enter first number: ";
+    cin>>num1;
+
+    cout<<"Enter second number: ";
+    cin>>num2;
+
+    cout<<"The addtion of "<<num1<<" and "<<num2<<" is "<<num1+num2<<endl;
+    cout<<"The substraction of "<<num1<<" and "<<num2<<" is "<<num1-num2<<endl;
+    cout<<"The Multiplication of "<<num1<<" and "<<num2<<" is "<<num1*num2<<endl;
+    cout<<"The Divison of "<<num1<<" and "<<num2<<" is "<<num1/num2<<endl;
+    cout<<"The modulus of "<<num1<<" and "<<num2<<" is "<<num1%num2<<endl;
+}
