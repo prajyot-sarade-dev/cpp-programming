26722431b595dda02963368828e8a0e78b8edce6
prajyot-sarade <prajyotsarade8@gmail.com>
2026-07-22 14:14:58 +0000

    swap of two numbers using third variable

---
+++ ex6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, temp;
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping:" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
